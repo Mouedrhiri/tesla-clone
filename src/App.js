05073b3df985eed8dd1cdc2cdcb59d95a42ede9d
Mohammed Ouedrhiri <mouedrhiri492@gmail.com>
2022-02-03 20:28:55 +0100

    Creating actual Tesla Clone Using React js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./componenets/Header";
+import Home from "./componenets/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
